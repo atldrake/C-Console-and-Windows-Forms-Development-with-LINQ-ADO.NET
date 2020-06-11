677209ab624bed93ac839016914c34c84007edb5
FirmTech <atldrake532@gmail.com>
2020-06-11 22:46:04 +0000

    StringManipulation

---
+++ StringManipulation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace StringManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare a String Variable
+
+
+            // Print To Screen
+
+
+            //Concatenation
+
+            //Count Length of String
+
+            //Replace Parts of Length
+
+            //Append to Existing String
+
+            //Strings Splits
+
+            //Compare Strings
+
+            //Convert to Strings
+
+            //
+        }
+    }
+}
