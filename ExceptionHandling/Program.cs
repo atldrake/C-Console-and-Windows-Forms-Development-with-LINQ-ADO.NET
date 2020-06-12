a479fcf04f274e3da175822852f184af105eebf1
FirmTech <atldrake532@gmail.com>
2020-06-12 21:42:08 +0000

    Exception Handling

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* try => a try block identifies a block of codes for which a particular Exception is activated
+             * Catch => A program catches an exeption with an exception handler
+             * finally = > the finally block is used to execute whether an exception is thrown or not
+             * throw => A program throws an exception when a problem shows up
+
+            */
+
+            Console.WriteLine("Enter a first number ");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a second number ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            try
+            {
+                int result = n1 / n2;
+                Console.WriteLine($"Result: {result}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Invalid operation entered: {e.Message}");
+
+            }
+            finally
+            {
+                Console.WriteLine("End of program");
+
+            }
+        }
+
+    }
+}
--- StringManipulation/Program.cs
+++ StringManipulation/Program.cs
@@ -7,24 +7,59 @@ namespace StringManipulation
         static void Main(string[] args)
         {
             //Declare a String Variable
-
+            string fullName = "My full name is Al-Husayn Abubakr";
+            string firstName = "Al-husayn";
+            string lastName = "Abubakar";
 
             // Print To Screen
-
+            Console.WriteLine(fullName);
 
             //Concatenation
-
+            Console.WriteLine("My full name is {0} {1} ", firstName, lastName);
+            Console.WriteLine("My full name is " + firstName + " " + lastName);
+            Console.WriteLine($"My full name is {firstName} {lastName}");
             //Count Length of String
+            int length = firstName.Length;
+            Console.WriteLine(length);
 
+            int length1 = fullName.Length;
+            Console.WriteLine(length1);
             //Replace Parts of Length
-
+            string newName = firstName.Replace('A', 'F');
+            string newName1 = lastName.Replace('a', 'o');
+            string newName2 = lastName.Replace("Al-husayn", "ATL Drake");
+            Console.WriteLine(newName);
+            Console.WriteLine(newName1);
+            Console.WriteLine(newName2);
             //Append to Existing String
 
             //Strings Splits
 
             //Compare Strings
+            if (firstName == lastName)
+            {
+                Console.WriteLine("First Name and Last Name is the same");
+            }
+            else
+            {
+                Console.WriteLine("First and Last are different");
+            }
+            int result = string.Compare(firstName, lastName);
+            if (result == 0)
+            {
+                Console.WriteLine("First Name and Last Name is the same");
+
+            }
+            else
+            {
+                Console.WriteLine("First and Last are different");
+
+            }
 
             //Convert to Strings
+             result = 123456789;
+            string bigNumber = result.ToString();
+            Console.WriteLine($"My bank Acnt number is {bigNumber}");
 
             //
         }
